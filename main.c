965609393df648cc10afaf2d5c9a27ad921593ce
Vinicius Bertuol <viniciusbertuol@alunos.utfpr.edu.br>
2022-06-03 17:22:22 -0300

    initial commmit

---
+++ main.c
@@ -0,0 +1,20 @@
+// Trabalho Semestral de Pesquisa e Classificação de Dados
+//
+// Alunos: Vinicius Bertuol e Gabriel Luiz Scariot
+// Link do Trabalho no Github: 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Trie {
+    int booleanTerminal;
+    int booleanLeaf;
+    Trie * pointerArray [26];
+}Trie;
+
+
+int main () {
+    
+    
+    return 0;
+}
